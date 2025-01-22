915562f41df09282a876e335485342c129a5b5b8
2dam <jhidalgo2377@gmail.com>
2025-01-22 11:39:14 +0100

    creada factoria de celda checkbox

--- NocturnaClient/src/ui_controllers/ShowAllArtistViewController.java
+++ NocturnaClient/src/ui_controllers/ShowAllArtistViewController.java
@@ -5,13 +5,19 @@
  */
 package ui_controllers;
 
-import exceptions.SignInException;
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 import javafx.application.Platform;
-import javafx.event.ActionEvent;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
@@ -19,14 +25,18 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.CheckBoxTableCell;
+import javafx.scene.control.cell.ChoiceBoxTableCell;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
-import logic.AdminManagerFactory;
-import logic.UserManagerFactory;
-import model.Admin;
+import logic.ArtistManagerFactory;
+import logic.EventManager;
+import logic.EventManagerFactory;
+import model.Artist;
+import model.Club;
 import model.Event;
 import model.User;
 import utils.CustomAlert;
@@ -62,6 +72,9 @@ public class ShowAllArtistViewController {
     TableColumn tcDescripcion;
 
     @FXML
+    TableColumn tcEventos;
+
+    @FXML
     Button btnSeleccionar;
 
     @FXML
@@ -77,6 +90,10 @@ public class ShowAllArtistViewController {
     private Event event;
 
     private boolean tema;
+
+    List<Artist> artists;
+
+    List<Artist> artistsCopia;
 
     private final Logger LOGGER = Logger.getLogger("SignInViewController.view");
 
@@ -101,29 +118,88 @@ public class ShowAllArtistViewController {
         Scene scene = new Scene(root);
 
         if (event == null) {
+            if (user.getIsAdmin()) {
+                tvArtists.setEditable(true);
+            }
             stage.setTitle("Visualizar artistas");
-            vbDepartamento.setVisible(false);
-            vbPass2.setVisible(false);
-            hbMenu.setVisible(false);
-            btnRegistrarAdmin.setVisible(false);
+
+            tcEventos.setText("¿Tiene eventos?");
+
         } else {
             stage.setTitle("Selector de artistas");
-            hlSignUp.setVisible(false);
-            hlRecuperarPass.setVisible(false);
-            btnSignIn.setVisible(false);
+            tcEventos.setText("¿Seleccionado?");
         }
 
-        btnSignIn.setOnAction(this::signIn);
-        btnRegistrarAdmin.setOnAction(this::registrarAdmin);
-        hlRecuperarPass.setOnAction(this::resetPass);
-        hlSignUp.setOnAction(this::signUp);
+        recogerArtistas();
+        cambiarTema();
+
         stage.setOnCloseRequest(this::closeAppFromX);
 
-        cambiarTema();
+        tfFiltroNombre.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                aplicarFiltros();
+            }
+        });
+
+        tfFiltroMusica.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                aplicarFiltros();
+            }
+        });
+
         stage.show();
         stage.setScene(scene);
         LOGGER.info("'ShowAllArtists' window initialized.");
 
+    }
+
+    private void recogerArtistas() {
+        artists = Arrays.asList(ArtistManagerFactory.get().findAll_XML(Artist[].class));
+    }
+
+    private void aplicarFiltros() {
+        artistsCopia = artists;
+
+        if (!tfFiltroNombre.getText().isEmpty()) {
+            artistsCopia = artists.stream().filter(artist -> artist.getNombre().startsWith(tfFiltroNombre.getText())).collect(Collectors.toList());
+        }
+
+        if (!tfFiltroMusica.getText().isEmpty()) {
+            artistsCopia = artists.stream().filter(artist -> artist.getTipoMusica().startsWith(tfFiltroMusica.getText())).collect(Collectors.toList());
+        }
+
+    }
+
+    private void cargarTabla(List<Event> tableEvents) {
+        initializeTableColumns();
+        // Convertir ArrayList a ObservableList
+        ObservableList<Event> observableEvents = FXCollections.observableArrayList(tableEvents);
+        // Cargar datos en la tabla
+        tablaEvent.setItems(observableEvents);
+
+    }
+
+    private void initializeTableColumns() {
+        tcNombre.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+        tcTipoMusica.setCellValueFactory(new PropertyValueFactory<>("tipoMusica"));
+        tcDescripcion.setCellValueFactory(new PropertyValueFactory<>("descripcion"));
+
+        tcEventos.setCellValueFactory(cellData -> {
+            Long artistId = cellData.getValue().getId(); // Obtener el ID del artista actual
+            boolean tieneEventos = comprobarEventos(artistId);
+            return new SimpleBooleanProperty(tieneEventos);
+        });
+        tcEventos.setCellFactory(CheckBoxTableCell.forTableColumn(tcEventos));
+
+    }
+
+    private boolean comprobarEventos(Long id) {
+        if (EventManagerFactory.get().findByArtist_XML(Event.class, String.valueOf(id)) != null) {
+            return true;
+        }
+        return false;
     }
 
     public void closeAppFromX(WindowEvent event) {
