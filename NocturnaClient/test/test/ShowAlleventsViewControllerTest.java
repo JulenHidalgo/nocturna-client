7804dcf537e34117d313eadc949da6b610a6c226
2dam <erlantzrey@gmail.com>
2025-02-03 10:08:11 +0100

    commit test crud Event

--- NocturnaClient/test/test/ShowAlleventsViewControllerTest.java
+++ NocturnaClient/test/test/ShowAlleventsViewControllerTest.java
@@ -8,16 +8,24 @@ package test;
 
 
 import control.Main;
+import control.Sesion;
+import static java.sql.Date.valueOf;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import javafx.scene.Node;
 import javafx.scene.control.TableView;
 import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
+import model.Artist;
+import model.Client;
 import model.Event;
+import model.User;
 import static org.testfx.api.FxAssert.verifyThat;
 import org.testfx.framework.junit.ApplicationTest;
 import static org.testfx.matcher.base.NodeMatchers.isInvisible;
 import static org.testfx.matcher.base.NodeMatchers.isVisible;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
@@ -28,58 +36,53 @@ import org.junit.runners.MethodSorters;
  */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ShowAlleventsViewControllerTest  extends ApplicationTest{
-    
+    /**
+     * tabla de eventos
+     */
     private TableView<Event> table;
     
     @Override
     public void start(Stage stage) throws Exception {
         new Main().start(stage);
-        table = lookup("#tablaEvent").query(); 
-        verifyThat("#btnAñadirArtistas",  isVisible());
-        verifyThat("#btnBorrarEvento",  isVisible());
-        verifyThat("#btnCrearEvento",  isVisible());
+        /**
+         * clickOn("#tfMail");
+         * write("");
+         * clickOn("#pfPass");
+         * write("");   
+         */
+        table = lookup("#tablaEvent").query();      
     }
     
-   /** @Test
-    public void testUserAdmin(){
-        
-        clickOn("#tfMail");
-        write("carlos@gamil.com");
-        
-        clickOn("#pfPass");
-        write("Password123");
-        
-        clickOn("#btnSignIn");
-        
-        verifyThat("#btnAñadirArtistas",  isVisible());
-        verifyThat("#btnBorrarEvento",  isVisible());
-        verifyThat("#btnCrearEvento",  isVisible());
-    }*/
-    
     /**
-     * comprueba que los evento se crean bien en la tabla 
+     * test para comprobar que tipo de usuario es 
      */
     @Test
-    public void test_A_CreateEvent(){
-        int rowCount=table.getItems().size();
-        
+    public void test_A_User() {
+        User user = Sesion.getUser();
+        if (user.getIsAdmin()) {
+            verifyThat("#btnAñadirArtistas", isVisible());
+            verifyThat("#btnBorrarEvento", isVisible());
+            verifyThat("#btnCrearEvento", isVisible());
+        } else {
+            verifyThat("#btnAñadirArtistas", isInvisible());
+            verifyThat("#btnBorrarEvento", isInvisible());
+            verifyThat("#btnCrearEvento", isInvisible());
+        }
+    }
+     
+    /**
+    * comprueba que los evento se crean bien en la tabla 
+    */
+    @Test
+    public void test_B_CreateEvent(){
+        int rowCount=table.getItems().size();       
         clickOn("#btnCrearEvento");
-        assertEquals("Evento creado correctamente",rowCount+1,table.getItems().size());
-        
+        assertEquals("Evento creado correctamente",rowCount+1,table.getItems().size());       
     }
     
-    /** @Test
-    public void test_B_AñadirArtist(){
-        int rowCount=table.getItems().size();
-        
-        clickOn("#btnAñadirArtistas");
-        
-        
-    }*/
-    
     /**
-     * comprueba si la tabla editable funciona bien
-     */
+    * comprueba si la tabla editable funciona bien
+    */
     @Test
     public void test_C_UpdateEvent() {
         
@@ -93,6 +96,29 @@ public class ShowAlleventsViewControllerTest  extends ApplicationTest{
         write("Viernes de Templo");
         push(KeyCode.ENTER);
         assertEquals("Nombre Modificado","Viernes de Templo", table.getItems().get(rowCount - 1).getNombre());
+        
+        // Abrir el ChoiceBox y seleccionar una opción
+        cell = lookup(".table-cell").nth((rowCount - 1) * table.getColumns().size() + 1).query();
+        doubleClickOn(cell);
+        clickOn(cell);
+        Node choiceBox = lookup(".choice-box").query();
+        clickOn(choiceBox);
+        clickOn("moma"); 
+        push(KeyCode.ENTER);
+        assertEquals("choiceBox modificado","moma", table.getItems().get(rowCount - 1).getClub().getNombre());
+
+        // Modificar la fecha y poner la de mañana
+        LocalDate tomorrow = LocalDate.now().plusDays(1);
+        String formattedDate = tomorrow.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        cell = lookup(".table-cell").nth((rowCount - 1) * table.getColumns().size() + 2).query();
+        doubleClickOn(cell);
+        clickOn(cell);
+        Node datePicker = lookup(".date-picker").query();
+        clickOn(datePicker);
+        eraseText(10);
+        write(formattedDate);
+        push(KeyCode.ENTER);
+        assertEquals("Fecha modificada", valueOf(tomorrow), table.getItems().get(rowCount - 1).getFecha());
         
         //modificar el precio
         cell = lookup(".table-cell").nth((rowCount - 1) * table.getColumns().size()+3).query();
@@ -112,13 +138,46 @@ public class ShowAlleventsViewControllerTest  extends ApplicationTest{
         push(KeyCode.ENTER);
         assertEquals("Numero de entradas modificado", 200, table.getItems().get(rowCount - 1).getNumEntradas());
         
+        //modificar la cantidad de consumiciones
+        cell = lookup(".table-cell").nth((rowCount - 1) * table.getColumns().size()+6).query();
+        doubleClickOn(cell);
+        clickOn(cell);
+        eraseText(1);
+        write("2");
+        push(KeyCode.ENTER);
+        assertEquals("Consumiciones modificado", 2, table.getItems().get(rowCount - 1).getConsumicion());         
     }
     
-     /**
-     * comprueba si la tabla editable funciona bien
-     */
+    /**
+    * test añadir artista a un evento
+    */
     @Test
-    public void test_D_DeleteEvent(){
+    public void test_D_AñadirArtist(){    
+        int rowCountE = table.getItems().size();
+        Node rowE = lookup(".table-row-cell").nth(rowCountE - 1).query();
+        clickOn(rowE);
+        clickOn("#btnAñadirArtistas");  
+        TableView<Artist> tvArtists = lookup("#tvArtists").query();
+        int rowCount = tvArtists.getItems().size();
+        int columnCount = tvArtists.getColumns().size();
+        if (rowCount != 0) {
+            Node cell = lookup(".table-cell").nth((columnCount - 1)).query();
+            doubleClickOn(cell);
+            clickOn(cell);
+            Node checkBox = lookup(".check-box").query();
+            clickOn(checkBox);
+            clickOn("#btnSeleccionar");
+            clickOn("Aceptar");
+            //comprobar que le tipoMusica a cambiado y ya no esta vacio
+            assertNotEquals("El Artista ya no es nulo", "No hay artistas", table.getItems().get(rowCount - 1 * table.getColumns().size()+5).getNombre());
+        }
+    }
+    
+    /**
+    * comprueba si la tabla editable funciona bien
+    */
+    @Test
+    public void test_F_DeleteEvent(){
         
         int rowCount = table.getItems().size();
         Node row = lookup(".table-row-cell").nth(rowCount - 1).query();
@@ -134,47 +193,8 @@ public class ShowAlleventsViewControllerTest  extends ApplicationTest{
         clickOn("#btnBorrarEvento");
         clickOn("Sí");
         clickOn("Aceptar");
-        assertEquals("Evento borrado",rowCount-1,table.getItems().size());
+        assertEquals("Evento borrado",rowCount-1,table.getItems().size()); 
         
-
     }
-    
-    /**public void testUserClient(){
-        
-        clickOn("#hbSignUp");
-        
-        clickOn("#tfNombre");
-        write("Carlos");
-
-        clickOn("#tfApellido");
-        write("Perez");
-
-        clickOn("#tfTelefono");
-        write("123456789");
-
-        clickOn("#dpFechaNac");
-        write("2000-01-01");
-
-        clickOn("#tfCiudad");
-        write("Madrid");
-
-        clickOn("#tfDni");
-        write("12345678A");
-
-        clickOn("#tfMail");
-        write("carlos@gamil.com");
-
-        clickOn("#pfPass");
-        write("Password123");
-
-        clickOn("#pfPass2");
-        write("Password123");
-        
-        clickOn("#btnSignUp");
-        
-        verifyThat("#btnAñadirArtistas",  isInvisible());
-        verifyThat("#btnBorrarEvento",  isInvisible());
-        verifyThat("#btnCrearEvento",  isInvisible());
-    }
-    */
+   
 }
